822b6c74f83bc8554485dc81e911079fafa77985
21p31a0415 <21p31a0415@acet.ac.in>
2025-05-08 19:31:55 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const parent = React.createElement("div",{id: "parent"},[
     React.createElement("div",{id: "child1"},[
         React.createElement("h1",{},"I'm am h1 Tag"),
-        React.createElement("h2",{},"I'm am h2 Tag")
+        React.createElement("h2",{},"wert5yuiojhgf"),
     ]),
     React.createElement("div",{id: "child2"},[
         React.createElement("h1",{},"I'm am h1 Tag"),
-        React.createElement("h2",{},"I'm am h2 Tag")
-    ])
-);
-
+        React.createElement("h2",{},"I'm am h2 Tag"),
+    ]),
+]);
+ 
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root")); 
 root.render(parent);
